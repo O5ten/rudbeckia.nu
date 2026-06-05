c2b17d0e0843d2c4c62dd832738ca29255136172
Mikael Östberg <mikael.ostberg@aurorainnovation.com>
2026-06-06 00:07:33 +0200

    add github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://test.rudbeckia.nu',
 	integrations: [
 		starlight({
+			plugins: [starlightThemeFlexoki({ accentColor: 'yellow' })],
 			title: 'Kollektivhuset Rudbeckia',
 			defaultLocale: 'sv',
 			locales: {
